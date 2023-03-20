1bd23e5a8858105d39ecb2b0af1edf7a1bf2c737
Igor Tatarintsev <igor.t@flowmapp.com>
2023-03-20 12:26:58 +0100

    Add webpack resolves config to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,11 @@ module.exports = {
   rules: {
     'max-len': ['error', { code: 120 }],
   },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'webpack.config.js',
+      },
+    },
+  },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,10 @@
 module.exports = {
   arrowParens: 'always',
   bracketSpacing: true,
-  htmlWhitespaceSensitivity: 'css',
-  bracketSameLine: false,
-  proseWrap: 'preserve',
-  quoteProps: 'as-needed',
   semi: false,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
   printWidth: 100,
-  embeddedLanguageFormatting: 'off',
+  embeddedLanguageFormatting: 'auto',
 };
